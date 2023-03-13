6b578db0cac03675bfbd1d3cb1ec9f8715c421fb
doctype-Melvin <doctype.melvin@gmail.com>
2023-03-13 19:28:53 +0100

    adds toggle fn for bookmarks

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 const bookmarks = [...document.querySelectorAll('.bookmark')]
 
-const toggleBookmark = (obj) => {
-    if (obj.className !== "bookmark saved") {
-        obj.classList.add('saved')
-        obj.attributes[1] = ''
-
-        obj.attributes[1] = 'src="./assets/bookmark.png'
-
-        // console.log(obj.attributes[1] = 'src="./assets/bookmark.png')
-    } else {
-        console.log('question saved')
-    }
-}
-
 bookmarks.forEach(item => item.addEventListener('click', (e) => {
-    let obj = e.target
-    toggleBookmark(obj)
+    if (e.target.classList.value !== "bookmark saved") {
+        e.target.classList.add('saved')
+        console.log(e.target.classList)
+    } else {
+        e.target.classList.remove('saved')
+        console.log(e.target.classList)
+    }
 }))
