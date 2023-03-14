c6a7fa5713abb2633be468220767a24503d3e8a4
Melvin Speckamp <doctype.melvin@gmail.com>
2023-03-14 12:34:58 +0100

    removes unneccesary functions in bookmarks eventL

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// Selects the grid container
 const cardsGrid = document.querySelector('[data-js="cardsGrid"]');
 
+//Function to create cards objects
 const elFactory = (type, attr, css, content, src) => {
   const element = document.createElement(type);
   element.setAttribute("data-js", attr);
@@ -11,6 +13,11 @@ const elFactory = (type, attr, css, content, src) => {
     element.textContent = "";
   }
   return element;
+};
+
+const cardFactory = () => {
+  const card = elFactory("section", "qCard", "q__card", "", "");
+  return card;
 };
 
 const questionCard = elFactory("section", "qCard", "q__card", "", "");
@@ -35,13 +42,6 @@ cardsGrid.append(questionCard);
 const bookmarks = [...document.querySelectorAll('[data-js="bookmarkIcon"]')];
 bookmarks.forEach((item) =>
   item.addEventListener("click", (e) => {
-    if (e.target.classList.value !== "bookmark saved") {
-      e.target.classList.add("saved");
-      console.log(e.target.classList);
-    } else {
-      e.target.classList.remove("saved");
-      console.log(e.target.classList);
-    }
+    e.target.classList.toggle("saved");
   })
 );
-console.log(bookmarks);
