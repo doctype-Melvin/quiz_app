8ed88970696d654bda39df2058641c53327b0fa8
Melvin Speckamp <doctype.melvin@gmail.com>
2023-03-16 13:41:43 +0100

    refactors cardFactory into individual functions

--- JS/main.js
+++ JS/main.js
@@ -19,23 +19,41 @@ const _data = [
   },
 ];
 
-const cardFactory = (data) => {
+// Section of element factories
+// to create individual elements
+const cardMaker = () => {
   const card = document.createElement("section");
   card.classList.add("quiz__card");
-  card.setAttribute("data-js", "qCard");
+  card.setAttribute("data-js", "quizCard");
+  return card;
+};
+
+const bookmarkMaker = () => {
   const bookmark = document.createElement("img");
   bookmark.classList.add("bookmark");
   bookmark.setAttribute("data-js", "bookmark");
   bookmark.src = "./assets/bookmark_saved.svg";
+  return bookmark;
+};
+
+const questionMaker = (data) => {
   const question = document.createElement("span");
   question.classList.add("card__question");
   question.setAttribute("data-js", "cardQuestion");
   question.textContent = data.question;
+  return question;
+};
+
+const buttonMaker = () => {
   const button = document.createElement("button");
-  button.classList.add("card__answer");
+  button.classList.add("card__answer__button");
   button.setAttribute("data-js", "cardAnswer");
   button.type = "submit";
   button.textContent = "Show Answer";
+  return button;
+};
+
+const tagsMaker = (data) => {
   const tagsContainer = document.createElement("ul");
   tagsContainer.classList.add("tags");
   tagsContainer.setAttribute("data-js", "tags");
@@ -44,6 +62,16 @@ const cardFactory = (data) => {
     tag.textContent = `#${element}`;
     tagsContainer.append(tag);
   });
+  return tagsContainer;
+};
+
+// Factory to create question cards
+const cardFactory = (data) => {
+  let card = cardMaker();
+  let bookmark = bookmarkMaker();
+  let question = questionMaker(data);
+  let button = buttonMaker();
+  let tagsContainer = tagsMaker(data);
 
   card.append(bookmark, question, button, tagsContainer);
   cardsGrid.append(card);
@@ -59,10 +87,12 @@ bookmarkIcons.forEach((btn) =>
   })
 );
 
-const answerButtons = [...document.querySelectorAll(".card__answer")];
+const answerButtons = [...document.querySelectorAll(".card__answer__button")];
 console.log(answerButtons);
 answerButtons.forEach((button) =>
   button.addEventListener("click", () => {
+    // Show answer on click
+    button.classList.add("card__answer__button--hidden");
     console.log("Answer button clicked!");
   })
 );
